4920b4ad34c1e58ce5f3ced0deac31781653e196
ihollander <ianholla@gmail.com>
2020-12-13 16:48:49 -0500

    Added bonus deliverable

--- src/data/blog.js
+++ src/data/blog.js
@@ -10,17 +10,20 @@ const blogData = {
       title: "Components 101",
       date: "December 15, 2020",
       preview: "Setting up the building blocks of your site",
+      minutes: 5,
     },
     {
       id: 2,
       title: "React Data Flow",
       date: "December 11, 2020",
       preview: "Passing props is never passé",
+      minutes: 15,
     },
     {
       id: 3,
       title: "Function vs Class Components",
       preview: "React, meet OOJS.",
+      minutes: 7,
     },
   ],
 };
