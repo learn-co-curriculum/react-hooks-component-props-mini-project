0b31089aba1f57ebf1a6a3cfcfac1e1e8db4ce11
ihollander <ianholla@gmail.com>
2021-04-06 14:38:17 -0400

    Fixed import syntax

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -1,6 +1,6 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
-import Header from "../components/header";
+import Header from "../components/Header";
 
 test("renders a <header> element", () => {
   const { container } = render(<Header />);
