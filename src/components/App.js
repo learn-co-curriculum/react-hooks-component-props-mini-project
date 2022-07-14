3c4b295ed0e88471bc76bf6e9a9ee6489df8a794
+dennis kiptoo <+dennis.kiptoo@student.moringaschool.com>
2022-07-14 20:49:33 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+import About from "./About";
+import ArticleList from "./ArticleList";
+import Header from "./Header";
 import blogData from "../data/blog";
 
 console.log(blogData);
@@ -6,8 +9,11 @@ console.log(blogData);
 function App() {
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      {/* You're on your own from here! Follow the deliverables; test things out in
+      the browser as you write your code; and good luck! */}
+      <Header name={blogData.name}/>
+      <About image={blogData.image}/>
+      <ArticleList posts={blogData.posts}/>
     </div>
   );
 }
--- src/data/blog.js
+++ src/data/blog.js
@@ -8,14 +8,14 @@ const blogData = {
     {
       id: 1,
       title: "Components 101",
-      date: "December 15, 2020",
+      date: "July 15, 2022",
       preview: "Setting up the building blocks of your site",
       minutes: 5,
     },
     {
       id: 2,
       title: "React Data Flow",
-      date: "December 11, 2020",
+      date: "July 14, 2022",
       preview: "Passing props is never passé",
       minutes: 15,
     },
