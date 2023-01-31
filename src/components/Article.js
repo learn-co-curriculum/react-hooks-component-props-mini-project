f07f156234c58e492dfe7e247e67eb6b6bcce538
Mayowa Ajiboye <mayaajiboye@gmail.com>
2023-01-31 16:56:23 -0500

    add emojis

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
-const Article = ({title, date ='January 1, 1970', preview }) =>{
+const Article = ({title, date ='January 1, 1970', preview, minutes }) => {
+
+  let minuteEmojis = ''
+  if(minutes < 30){
+    let roundedMins = Math.ceil(minutes/5)
+    minuteEmojis = "☕️".repeat(roundedMins)
+
+  } else {
+    let roundedMins = Math.ceil(minutes/10)
+    minuteEmojis = "🍱".repeat(roundedMins)
+  }
   return(
     <article>
       <h3>{title}</h3>
       <small>{date}</small>
       <p>{preview}</p>
+      <p>{minuteEmojis}</p>
     </article>
 
   )
