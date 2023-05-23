bbc2cca6c496d09dbc0a8c3a049182ec68d86705
Macolister <bispo.mac@gmail.com>
2023-05-23 19:15:37 -0400

    save About.js

--- src/components/About.js
+++ src/components/About.js
@@ -2,16 +2,15 @@ import React from "react";
 
 function About ({image = "https://via.placeholder.com/215", alt, about}){
 
-    console.log(image)
-    console.log(alt)
-    console.log(about)
+    // console.log(image)
+    // console.log(alt)
+    // console.log(about)
 
     return(
         <aside>
             <img src={image} alt ={alt}/>
             <p>{about}</p>
         </aside>
-
     );
 
 
