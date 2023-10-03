339bea9ade687bf43a36bf6ef3eeb3bb26ab46e4
LingZhouChen <lingzhouc@gmail.com>
2023-10-03 10:34:22 -0400

    header deliverable

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,17 @@
 import React from "react";
 import blogData from "../data/blog";
+import Header from "./Header";
 
 console.log(blogData);
 
 function App() {
   return (
-    <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
-    </div>
+    // <div className="App">
+    //   You're on your own from here! Follow the deliverables; test things out in
+    //   the browser as you write your code; and good luck!
+    // </div>
+
+    <Header name={blogData.name}/>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header(prop) {
+    return(
+        <header>
+            <h1>{prop.name}</h1>
+        </header>
+    )
+}
+
+export default Header;
