e7f4a36093fb25ece7153d6907abbe43ba80e8dd
Jim Grimes <jimgrimes86@gmail.com>
2023-10-03 12:32:50 -0400

    DONE bonus feature

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -2,7 +2,7 @@ import Article from "./Article"
 
 function ArticleList({articles}) {
     const articleList = articles.map((article) => {
-        return <Article key={article.id} title={article.title} date={article.date} preview={article.preview}/>
+        return <Article key={article.id} title={article.title} date={article.date} preview={article.preview} minutes={article.minutes}/>
     })
 
     return (
