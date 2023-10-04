f8bed762b36b17e994c5d2d45719dd37017f3362
LingZhouChen <lingzhouc@gmail.com>
2023-10-04 13:27:01 -0400

    rewrote Article deliverable

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -3,8 +3,17 @@ import Article from "./Article";
 
 function ArticleList( {posts} ) {
 
+    // console.log(posts)
+
     const postItems = posts.map((post)=>{
-        return <Article key={post.id}/>
+        return (
+            <Article 
+            key={post.id} 
+            title={post.title} 
+            date={post.date} 
+            preview={post.preview}
+            />
+        )
     })
 
   return (
