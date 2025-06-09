63882fd8fda6d7b5618c1d3d2f70af77073fdecf
Mike <mclancy96@gmail.com>
2025-06-09 15:37:37 -0400

    Header added

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import blogData from "../data/blog";
+import Header from "./Header";
+import About from "./About";
 
 console.log(blogData);
 
 function App() {
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      <Header name="Mike's Page" />
+      <About image="https://upload.wikimedia.org/wikipedia/en/7/7b/MaxheadroomMpegMan.jpg" />
     </div>
   );
 }
