bd955c3942b0d4098f5dc6119989d0b9a75b1bd4
Mike <mclancy96@gmail.com>
2025-06-09 15:38:39 -0400

    About passing

---
+++ src/components/About.jsx
@@ -0,0 +1,10 @@
+const About = ({ image = "https://via.placeholder.com/215", about }) => {
+  return (
+    <aside>
+      <img src={image} alt="blog logo" />
+      <p>{about}</p>
+    </aside>
+  );
+};
+
+export default About;
