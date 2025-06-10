896353ff5351d2633151eeb7ec9ce903e9307584
Emily Kanarek <emilyakanarek@gmail.com>
2025-06-10 11:23:51 -0400

    List articles

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import blogData from "../data/blog";
 import Header from "./Header";
 import About from "./About";
+import ArticleList from "./ArticleList";
 
 console.log(blogData);
 
@@ -9,7 +10,8 @@ function App() {
   return (
     <div className="App">
       <Header name={blogData.name} />
-      <About image={blogData.image} about={blogData.about}/>
+      <About image={blogData.image} about={blogData.about} />
+      <ArticleList posts={blogData.posts} />
     </div>
   );
 }
---
+++ src/components/ArticleList.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Article from "./Article";
+
+function ArticleList({ posts }) {
+  return (
+    <main>
+      {posts.map((post) => (
+        <Article
+          key={post.id}
+          title={post.title}
+          date={post.date}
+          preview={post.preview}
+        />
+      ))}
+    </main>
+  );
+}
+
+export default ArticleList;
