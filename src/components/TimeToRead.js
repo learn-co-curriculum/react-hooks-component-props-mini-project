471d00de997a36d590eff40432a7cfb0ddb2de78
Emily Kanarek <emilyakanarek@gmail.com>
2025-06-10 12:00:40 -0400

    Display emojis for number of minutes

--- src/components/TimeToRead.js
+++ src/components/TimeToRead.js
@@ -1,9 +1,28 @@
 import React from "react";
 
 function TimeToRead({ minutes }) {
-  console.log(minutes)
+  const appendEmojis = (minutes) => {
+    if (minutes >= 30) {
+      let bentos = "🍱🍱";
+      const roundedMins = Math.ceil(minutes / 10) * 10;
+      for (let i = 30; i <= roundedMins; i += 10) {
+        bentos += "🍱"
+      }
+      return bentos;
+    } else {
+      let coffees = "";
+      const roundedMins = Math.ceil(minutes / 5) * 5;
+      for (let i = 5; i <= roundedMins; i += 5) {
+        coffees += "☕️"
+      }
+      return coffees;
+    }
+  }
+
+  const emojis = appendEmojis(minutes)
+  
   return (
-  <small>{minutes} min read</small>
+  <small>{emojis} {minutes} min read</small>
  )
 }
 
