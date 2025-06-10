061cfa97072d756880e020359ef33b93a7ade3e9
Emily Kanarek <emilyakanarek@gmail.com>
2025-06-10 11:10:18 -0400

    Add header with blog name

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import blogData from "../data/blog";
+import Header from './Header'
 
 console.log(blogData);
 
 function App() {
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      <Header name={blogData.name}/>
     </div>
   );
 }
