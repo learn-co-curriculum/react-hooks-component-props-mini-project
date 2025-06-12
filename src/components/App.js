bf6cde5aaa4e7a94cbd25e0f3b804822306222e3
Jen Kelly <jen.kelly@powerhrg.com>
2025-06-12 12:01:55 -0400

    Create About component and pass blogData variables from App

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,16 @@
 import React from "react"
 import blogData from "../data/blog"
 import Header from "./Header"
+import About from "./About"
 
 console.log(blogData)
 
 function App() {
-  const {name} = blogData
+  const {name, image, about} = blogData
   return (
     <div className="App">
       <Header name={name} />
+      <About image={image} about={about} />
     </div>
   )
 }
