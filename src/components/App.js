fdf82b643afe3d02b624006fbd3e8f067daee9e1
Jen Kelly <jen.kelly@powerhrg.com>
2025-06-12 11:44:02 -0400

    Create Header component and pass name variable from App

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,16 @@
-import React from "react";
-import blogData from "../data/blog";
+import React from "react"
+import blogData from "../data/blog"
+import Header from "./Header"
 
-console.log(blogData);
+console.log(blogData)
 
 function App() {
+  const {name} = blogData
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      <Header name />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
